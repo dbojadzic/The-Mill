47c4fd0fb8d4e24916a308d38ab2d175fb6a1461
dbojadzic <dbojadzic1@etf.unsa.ba>
2019-01-26 19:35:01 +0100

    Commit of the project files for The-Mill game.

---
+++ WindMill/Assets/Scripts/FanMenuRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanMenuRotation : MonoBehaviour {
+    public int speed = 10;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+    }
+}
---
+++ WindMill/Assets/Scripts/LightScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    // Use this for initialization
+    private Light flashlight;
+	void Start () {
+        flashlight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.F))        
+            flashlight.enabled = !flashlight.enabled;
+        
+    }
+}
